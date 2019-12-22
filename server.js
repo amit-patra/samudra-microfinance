a3b5be09f07836d609dd4fd26ad462e9a2fe5918
amit-9036763618 <33975358+amit-9036763618@users.noreply.github.com>
2019-12-22 23:29:43 +0530

    make it better

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT | 8080);
+app.use(express.static(__dirname + '/dist/samudraMicrofinance'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/samudraMicrofinance/index.html'));
 })
+app.listen(process.env.PORT || 8080);
 console.log('console listening!');
